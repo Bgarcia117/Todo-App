c065082ff0b89b98b97990faee3f7c56ce289d8e
Bgarcia117 <bryant.garcia.1771@gmail.com>
2023-01-17 21:20:37 -0600

    Optimized code to allow user to add items in a single line.

--- main.py
+++ main.py
@@ -5,66 +5,68 @@ while True:
     user_action = user_action.strip()
     # Using the strip method like this allows us to keep the changes to spacings when the variable is called again.
 
-    match user_action:
-        case 'add':
-            todo = input("Enter a todo: ") + "\n"
+    
+    if 'add' in user_action or 'new' in user_action:
+        todo = user_action[4:] + '\n'
+        # This is a list slice 
 
-            with open('Files/todos.txt', 'r') as file:
-                 todos = file.readlines()
-                # This creates a list by reading what is on that file. The benefit of using with and as is that you do not need close the file. (As file is works like "file =")
-
-            todos.append(todo) 
-
-            with open('Files/todos.txt', 'w') as file:
-                file.writelines(todos)
-            
-        case 'show' | 'display':
-        # This is a bitwise or operator
-
-            with open('Files/todos.txt', 'r') as file:
+        with open('Files/todos.txt', 'r') as file:
                 todos = file.readlines()
+            # This creates a list by reading what is on that file. The benefit of using with and as is that you do not need close the file. (As file is works like "file =")
 
-            # new_todos = [item.strip('\n') for item in todos]
-            # This is a list comprehension. It works like a for & in
+        todos.append(todo) 
 
-            for index, item in enumerate(todos):
-            # Enumerate adds counter to each item in the list
-            #Use 'for # in #' to display a list without brackets
-                # item.capatlize()
-                item = item.strip('\n')
-                row = f"{index + 1}-{item}"
-                print(row)
-        case 'edit':
-            number = int(input("Enter the number of the todo item to edit: "))
-            number = number - 1
+        with open('Files/todos.txt', 'w') as file:
+            file.writelines(todos)
+        
+    elif 'show' in user_action:
+    # elif stops the program from checking these blocks of code when it executes one
 
-            with open('Files/todos.txt', 'r') as file:
-                todos = file.readlines()
+        with open('Files/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            new_todo = input("Enter a new todo: ")
-            todos[number] = new_todo
+        # new_todos = [item.strip('\n') for item in todos]
+        # This is a list comprehension. It works like a for & in
 
-            with open('Files/todos.txt', 'w') as file:
-                file.writelines(todos)
+        for index, item in enumerate(todos):
+        # Enumerate adds counter to each item in the list
+        #Use 'for # in #' to display a list without brackets
+            # item.capatlize()
+            item = item.strip('\n')
+            row = f"{index + 1}-{item}"
+            print(row)
 
-        case "complete":
-            number = int(input("Enter the number of the todo item to complete: "))
+    elif 'edit' in user_action:
+        number = int(user_action[5:])
+        number = number - 1
 
-            with open('Files/todos.txt', 'r') as file:
-                todos = file.readlines()
-            index = number - 1
-            todo_to_remove = todos[index.strip('\n')]
-            todos.pop(number - 1)
-            # Pop removes an item using its index. 
+        with open('Files/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            with open('Files/todos.txt', 'w') as file:
-                file.writelines(todos)
-            
-            message = f"Todo {todo_to_remove} was removed from the list"
-            print(message)
+        new_todo = input("Enter a new todo: ")
+        todos[number] = new_todo
 
-        case 'exit':
-            break
-        # case _:
-        #     print("Hey you entered an unknown command.")
+        with open('Files/todos.txt', 'w') as file:
+            file.writelines(todos)
+
+    elif "complete" in user_action:
+        number = int(user_action[9:])
+
+        with open('Files/todos.txt', 'r') as file:
+            todos = file.readlines()
+        index = number - 1
+        todo_to_remove = todos[index].strip('\n')
+        todos.pop(index)
+        # Pop removes an item using its index. 
+
+        with open('Files/todos.txt', 'w') as file:
+            file.writelines(todos)
+        
+        message = f"Todo {todo_to_remove} was removed from the list"
+        print(message)
+
+    elif 'exit' in user_action:
+        break
+    # case _:
+    #     print("Hey you entered an unknown command.")
 
