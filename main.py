91b065d000824c75bcce2dec323e695129fad451
Bgarcia117 <bryant.garcia.1771@gmail.com>
2023-02-17 20:49:48 -0600

    created a function to reduce repetition in the code.

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 prompt = "Type add, show, edit, complete, or exit:"
 
+def get_todos():
+    with open('Files/todos.txt', 'r') as file_local:
+                todos_local = file_local.readlines()
+                 # This creates a list by reading what is on that file. The benefit of using with and as is that you do not need close the file. (As file_local works like "file =")
+    return todos_local
+
+            
 while True:
     user_action = input(prompt)
     user_action = user_action.strip()
     # Using the strip method like this allows us to keep the changes to spacings when the variable is called again.
 
     
-    if 'add' in user_action or 'new' in user_action:
-        todo = user_action[4:] + '\n'
+    if user_action.startswith("add"):
+        todo = user_action[4:]
         # This is a list slice 
 
-        with open('Files/todos.txt', 'r') as file:
-                todos = file.readlines()
-            # This creates a list by reading what is on that file. The benefit of using with and as is that you do not need close the file. (As file is works like "file =")
-
-        todos.append(todo) 
+        todos = get_todos()
+           
+        todos.append(todo + '\n') 
 
         with open('Files/todos.txt', 'w') as file:
             file.writelines(todos)
         
-    elif 'show' in user_action:
+    elif user_action.startswith("show"):
     # elif stops the program from checking these blocks of code when it executes one
 
-        with open('Files/todos.txt', 'r') as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         # new_todos = [item.strip('\n') for item in todos]
         # This is a list comprehension. It works like a for & in
@@ -36,36 +40,44 @@ while True:
             row = f"{index + 1}-{item}"
             print(row)
 
-    elif 'edit' in user_action:
-        number = int(user_action[5:])
-        number = number - 1
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            # print(number) 
+            number = number - 1
 
-        with open('Files/todos.txt', 'r') as file:
-            todos = file.readlines()
+            todos = get_todos()
 
-        new_todo = input("Enter a new todo: ")
-        todos[number] = new_todo
+            new_todo = input("Enter a new todo: ")
+            todos[number] = new_todo
 
-        with open('Files/todos.txt', 'w') as file:
-            file.writelines(todos)
+            with open('Files/todos.txt', 'w') as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Your command is not valid.")
+            continue
 
-    elif "complete" in user_action:
-        number = int(user_action[9:])
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
 
-        with open('Files/todos.txt', 'r') as file:
-            todos = file.readlines()
-        index = number - 1
-        todo_to_remove = todos[index].strip('\n')
-        todos.pop(index)
-        # Pop removes an item using its index. 
+            todos = get_todos()
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+            # Pop removes an item using its index. 
 
-        with open('Files/todos.txt', 'w') as file:
-            file.writelines(todos)
-        
-        message = f"Todo {todo_to_remove} was removed from the list"
-        print(message)
+            with open('Files/todos.txt', 'w') as file:
+                file.writelines(todos)
+            
+            message = f"Todo {todo_to_remove} was removed from the list"
+            print(message)
 
-    elif 'exit' in user_action:
+        except IndexError:
+            print("There is no item with that number.")
+            continue
+
+    elif user_action.startswith("exit"):
         break
     # case _:
     #     print("Hey you entered an unknown command.")
